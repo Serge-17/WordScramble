c7f84d9aebd3378b87ce5f6d7028c852c7a66ac0
Serge-17 <sergei.eliseev17@icloud.com>
2024-09-19 10:52:27 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Serge Eliseev on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
